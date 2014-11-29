43d0bbbea19642fde44ea0ec5fe4a0fdfa362bd6
Erick Dos Ramos <erick2ramos@gmail.com>
2014-11-29 18:59:21 -0430

    Hace cosas (no necesariamente bien) pa lante con esto falta el informe.

--- src/main.m
+++ src/main.m
@@ -1,7 +1,9 @@
-source("utils.m");
+source('utils.m');
 
-data = dlmread("../data/iris.data.modified3class.txt");
+arg_list = argv();
 
+data = dlmread('../data/iris.data.modified3class.txt');
+errorMsg = 'Uso: main [seleccion] [fitness] [tasa mutacion] [tasa cruce]\nseleccion: [1 (Por rango),2 (Rueda de ruleta)]\nfitness: [1 (normal), 2 (penalizacion por tamano)]\ntasa mutacion: (0.0:1.0)\ntasa cruce: (0.0:1.0)\n';
 %figure(1);
 %subplot(2,2,1);
 %hist(data(:,1),30);
@@ -14,58 +16,107 @@ data = dlmread("../data/iris.data.modified3class.txt");
 tic();
 LB = min(data);
 UB = max(data);
-bitsPerFeature = [ 4 3 2 3 2 ];
+bitsPerFeature = [ 8 6 5 3 2 ];
 Nvar = length(bitsPerFeature);
 featureTotal = sum(bitsPerFeature);
 
 
-validationPool = data(randperm(size(data,1))(1:30),:);
+validationPool = data(randperm(size(data,1),75),:);
 examples = [];
 for i = 1:size(validationPool,1)
     examples = [examples; encode(validationPool(i,:), bitsPerFeature, LB, UB)];
 endfor
 
-Npop = 10;
+Npop = 20;
 population = [];
 for i = 1:Npop
     newpep = [];
-    for j = 1:ceil(rand * 4)
+    for j = 1:ceil(rand * 3)
         newpep = [newpep randCoded(featureTotal)];
     endfor
     population = [population; newpep];
 endfor
-fsum = zeros(1, Npop);
-for i = 1:Npop
-    fsum(i) = fitnessFn(population(i,:), featureTotal, examples);
-endfor
 
 maxGenerations = 100;
-mutationRate = 0.2;
-selectionPercent = 0.5;
+if length(arg_list) < 3 
+    mutationRate = 0.1;
+else
+    if str2num(arg_list{3}) > 0 && str2num(arg_list{3}) < 1
+        mutationRate = str2num(arg_list{3});
+    else
+        fprintf(errorMsg);
+        exit
+    endif
+endif
+
+if length(arg_list) < 4
+    selectionPercent = 0.5;
+else
+    if str2num(arg_list{4}) > 0 && str2num(arg_list{4}) < 1
+        selectionPercent = str2num(arg_list{4});
+    else
+        fprintf(errorMsg);
+        exit
+    endif
+endif
+
 nTrascendence = floor(selectionPercent * Npop);
 nMutations = ceil((Npop - 1)*Nvar*mutationRate);
 nMatings = ceil((Npop - nTrascendence)/2);
-maxFitness = 5;
+maxFitness = 0.98;
 
-selection = @selectionFn;
+if length(arg_list) >= 2
+    if !(str2num(arg_list{2}) == 1 || str2num(arg_list{2}) == 2)
+        fprintf(errorMsg);
+        exit
+    endif
+    if str2num(arg_list{2}) == 2
+        fitness = @fitnessSizeFn;
+    else
+        fitness = @fitnessFn;
+    endif
+else
+    fitness = @fitnessFn;
+endif
+
+if length(arg_list) >= 1
+    if !(str2num(arg_list{1}) == 1 || str2num(arg_list{1}) == 2)
+        fprintf(errorMsg);
+        exit
+    endif
+    if str2num(arg_list{1}) == 2
+        selection = @selectionRouletteFn;
+    else
+        selection = @selectionFn;
+    endif
+else
+    selection = @selectionFn;
+endif
+
 crossover = @crossoverFn;
 mutation = @mutationFn;
 
-tFsum = cell(maxGenerations,1);
+fsum = zeros(1, Npop);
+for i = 1:Npop
+    fsum(i) = fitness(population(i,:), featureTotal, examples);
+endfor
 
 gen = 0;
-while max(fsum) < maxFitness && gen < maxGenerations
+while  max(fsum) < maxFitness && gen < maxGenerations
     newGen = [];
     newGen = selection(population, fsum, nTrascendence);
-    newGen = [newGen; crossover(population, fsum, bitsPerFeature, nMatings)];
+    newGen = crossover(newGen, bitsPerFeature, nMatings);
     newGen = mutation(newGen, mutationRate);
-    population = newGen;
-    tFsum{gen + 1} = max(fsum);
-    fsum = zeros(1, size(population,1));
-    for i = 1:size(population,1)
-        fsum(i) = fitnessFn(population(i,:), featureTotal, examples); 
-    endfor
-    tFsum = [ tFsum; fsum ];
+    newSum = zeros(1, size(newGen,1));
+    for i = 1:size(newGen,1)
+        newSum(i) = fitness(newGen(i,:), featureTotal, examples); 
+    endfor 
+    [ fsum, ind ] = sort(fsum, 'descend');
+    population = population(ind,:);
+    population = population(1:size(population,1) - size(newGen,1),:);
+    population = [population; newGen];
+    fsum = fsum(1:length(fsum) - length(newSum));
+    fsum = [fsum, newSum];
     Npop = size(population,1);
     nTrascendence = floor(selectionPercent * Npop);
     nMutations = ceil((Npop - 1)*Nvar*mutationRate);
@@ -74,7 +125,4 @@ while max(fsum) < maxFitness && gen < maxGenerations
 endwhile
 toc()
 [x, index] = max(fsum);
-population(index,:)
-%figure(1);
-%plot(0:length(tFsum),tFsum{:})
-gen
+fprintf('Mejor individuo: %s\nAcertadas: %f\nGeneracion: #%d\n',cellstr(population(index,:)){1}, max(fsum) * 100, gen);
